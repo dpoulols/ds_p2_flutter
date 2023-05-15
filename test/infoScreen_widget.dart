7df35d4b02646ff777d30adc28a802b1a721feb7
Victor Perez Barranco <58432084+VictorPB@users.noreply.github.com>
2023-05-15 21:52:49 +0200

    Test widget basico que pasa

---
+++ test/infoScreen_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ds_p2_flutter/informationScreen.dart';
+
+void main() {
+  testWidgets('InformationScreen displays mueble information', (WidgetTester tester) async {
+    final widget = InformationScreen(mueble: 'silla', tipo: 'oficina', material: 'madera');
+    await tester.pumpWidget(MaterialApp(home: widget));
+
+    expect(find.text('Información del producto'), findsOneWidget);
+    expect(find.text('Información sobre la silla'), findsOneWidget);
+    expect(find.text('Tipo: oficina'), findsOneWidget);
+    expect(find.text('Material: madera'), findsOneWidget);
+    expect(find.text('Precio: 24.0€'), findsOneWidget);
+    expect(find.text('Garantía: 2 años'), findsOneWidget);
+  });
+}
